9f86897ffbc8a720e36c52d65fd99df76765ae7c
Del <aelbanna3896@gmail.com>
2025-03-29 03:06:23 -0400

    Change order of drawing to draw stats on top Change order of calls in update to activate keybinds first Add display more stats Change movement control approach Add speed cap Improve world boundary collision Add code to automatically return particle to world bounds

--- particlesim.js
+++ particlesim.js
@@ -10,9 +10,10 @@ let textMetrics = ctx.measureText("A");
 let fontHeight = textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent;
 
 const deltaTime = 1 / 60;
-const frictionCoefficient = 0.006;
-const acceleration = 100 * Math.E;
-const maxSpeed = 800;
+const frictionCoefficient = 0.012;
+const acceleration = 1200 * Math.E,
+    diagonalAccelerationComponent = acceleration * Math.SQRT1_2;
+const maxSpeed = 12000;
 
 
 let lastFrameTime = null;
@@ -35,11 +36,13 @@ let worldBounds = {
     bottom: canvasHeight - 10,
     getCenterX: () => (worldBounds.right - worldBounds.left) / 2,
     getCenterY: () => (worldBounds.bottom - worldBounds.top) / 2,
+    getWidth: () => worldBounds.getCenterX() - worldBounds.left,
+    getHeight: () => worldBounds.getCenterY() - worldBounds.top
 }
 
 let particle1 = {
     color: "red",
-    size: 3,
+    size: 10,
     mass: 10,
     x: worldBounds.getCenterX(),
     y: worldBounds.getCenterY(),
@@ -47,12 +50,14 @@ let particle1 = {
     vY: 0,
     aX: 0,
     aY: 0,
+    daX: 0,
+    daY: 0,
+    computedSpeed: 0,
+    getAccelerationMagnitude: () => {
+        return Math.sqrt(particle1.aX ** 2 + particle1.aY ** 2);
+    },
     getSpeedSquared: () => {
         return particle1.vX * particle1.vX + particle1.vY * particle1.vY;
-    },
-    getNormalizedVelocityVector: () => {
-        let ss = Math.sqrt(particle1.getSpeedSquared());
-        return [particle1.vX / ss, particle1.vY / ss];
     }
 }
 
@@ -61,27 +66,28 @@ let keyStates = {
 
 let bindings = {
     up: {
+        type: "movement",
         keys: ["w"],
         action: () => {
-            particle1.aY = -1 * acceleration ** 2 / Math.sqrt(acceleration ** 2 + particle1.aX ** 2);
+            particle1.daY += -1;
         }
     },
     down: {
         keys: ["s"],
         action: () => {
-            particle1.aY = acceleration ** 2 / Math.sqrt(acceleration ** 2 + particle1.aX ** 2);
+            particle1.daY += 1;
         }
     },
     left: {
         keys: ["a"],
         action: () => {
-            particle1.aX = -1 * acceleration ** 2 / Math.sqrt(particle1.aY ** 2 + acceleration ** 2);
+            particle1.daX += -1;
         }
     },
     right: {
         keys: ["d"],
         action: () => {
-            particle1.aX = acceleration ** 2 / Math.sqrt(particle1.aY ** 2 + acceleration ** 2);
+            particle1.daX += 1;
         }
     }
 };
@@ -124,17 +130,47 @@ function differenceSquared(a, b) {
 }
 
 function updateVelocity(particle) {
-    if (differenceSquared(particle.x, worldBounds.left) <= particle.size || differenceSquared(particle.x, worldBounds.right) <= particle.size)
-        particle.vX *= -1;
+    let { x, y, vX, vY, aX, aY, size } = particle;
+    let worldWidth = worldBounds.getWidth(),
+        worldHeight = worldBounds.getHeight();
+    let distanceToCenterX = Math.abs(worldBounds.getCenterX() - x),
+        distanceToCenterY = Math.abs(worldBounds.getCenterY() - y);
 
-    if (differenceSquared(particle.y, worldBounds.top) <= particle.size || differenceSquared(particle.y, worldBounds.bottom) <= particle.size)
-        particle.vY *= -1;
+    if (distanceToCenterX > worldWidth - size) {
+        let direction = Math.sign(worldBounds.getCenterX() - x);
+        particle.vX = direction * (Math.abs(vX) + 3 * (distanceToCenterX - worldWidth) * deltaTime);
+    }
 
-    particle.vX += particle.aX * deltaTime;
-    particle.vY += particle.aY * deltaTime;
+    if (distanceToCenterY > worldHeight - size) {
+        let direction = Math.sign(worldBounds.getCenterY() - y);
+        particle.vY = direction * (Math.abs(vY) + 3 * (distanceToCenterY - worldHeight) * deltaTime);
+    }
+
+    particle.vX += aX * deltaTime;
+    particle.vY += aY * deltaTime;
+
+    let speed = particle.computedSpeed = Math.sqrt(vX ** 2 + vY ** 2);
+
+    if (speed > maxSpeed) {
+        let factor = maxSpeed / speed;
+        particle.vX *= factor;
+        particle.vY *= factor;
+        particle.computedSpeed = maxSpeed;
+    }
 }
 
 function updateAcceleration(particle) {
+    let { daX, daY } = particle;
+    let magnitude = Math.sqrt(daX ** 2 + daY ** 2);
+    if (magnitude) {
+        particle.aX = acceleration * daX / magnitude;
+        particle.aY = acceleration * daY / magnitude;
+    } else {
+        particle.aX = 0;
+        particle.aY = 0;
+    }
+    particle.daX = 0;
+    particle.daY = 0;
 }
 
 function updatePosition(particle) {
@@ -152,20 +188,22 @@ function updateMotion() {
     updateVelocity(particle1);
     updatePosition(particle1);
     applyFriction(particle1, frictionCoefficient);
+    particle1.aX = 0;
+    particle1.aY = 0;
 }
 
 function activateKeyBindings() {
     for (const binding of Object.values(bindings)) {
-        if (binding.keys.map(k => keyStates[k]).includes(true))
+        if (binding.keys.some(k => keyStates[k]))
             binding.action();
     }
 }
 
 // Game logic
 function update() {
+    activateKeyBindings();
     updateStats();
     updateMotion();
-    activateKeyBindings();
 }
 
 function drawComplexText(x, y, content = [["Colored ", "red"], ["\n"], ["Text ", "Blue"], ["Test", "Green"]], lineSpacing = 2) {
@@ -190,17 +228,17 @@ function drawComplexText(x, y, content = [["Colored ", "red"], ["\n"], ["Text ",
 }
 
 function drawStats() {
+    const { getAccelerationMagnitude, getSpeedSquared, aX, aY, daX, daY, ...particleStats } = particle1;
     drawComplexText(10, 10,
         [
-            ["FPS: " + stats.fps(), "white"],
-            ["\n"],
+            [`FPS: ${stats.fps()}\n`, "white"],
             ["Animate: ", "white"],
             isAnimating ? ["on", "green"] : ["off", "red"],
             ["\n"],
-            ["Acceleration: ", "white"],
-            ["" + Math.sqrt(particle1.aX ** 2 + particle1.aY ** 2), "white"]
+            ...Object.entries(particleStats).map(([key, val]) => [`${key}: ${parseFloat(val + "") ? Math.round(val * 100) / 100 : val}\n`, "white"]),
+            // [`Acceleration: ${particle1.getAccelerationMagnitude()}\n`, "white"],
         ],
-        2)
+        2);
 }
 
 function drawParticle() {
@@ -213,15 +251,15 @@ function drawParticle() {
 // Rendering
 function draw() {
     clearCanvas();
+    drawParticle();
     if (isStatsVisible)
         drawStats();
-    drawParticle();
 }
 
 // Game loop
 function animate() {
-    update();
     draw();
+    update();
     if (isAnimating)
         requestAnimationFrame(animate);
 }
