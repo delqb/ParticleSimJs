81d9d88dca665f7530679ed9b95e9d5ec435500a
Del <aelbanna3896@gmail.com>
2025-05-28 14:07:11 -0400

    Move Entity, Component, System, Node, and Engine to the engine module.

--- engine.ts
+++ engine.ts
@@ -34,3 +34,148 @@ export const Vector2 = {
         return { x: v.x, y: v.y };
     }
 }
+
+export type Component = {
+    key: string;
+};
+
+export type Node = {
+    [key: string]: Component
+};
+
+
+export class Entity {
+    private id: EntityID = createUID();
+    private components = new Map<string, Component>();
+
+    getID(): EntityID {
+        return this.id;
+    }
+
+    hasComponents(keys: string[]): boolean {
+        return keys.every(key => this.components.has(key));
+    }
+
+    getComponent<T extends Component>(key: string): T | undefined {
+        return this.components.get(key) as T;
+    }
+
+    addComponent<T extends Component>(component: T): void {
+        this.components.set(component.key, component);
+    }
+
+    removeComponent(key: string): boolean {
+        return this.components.delete(key);
+    }
+
+    getComponentMap(): Map<string, Component> {
+        return this.components;
+    }
+}
+
+export abstract class System<T extends Node> {
+    abstract readonly NODE_COMPONENT_KEYS: Set<Extract<keyof T, string>>;
+    private nodeMap: Map<EntityID, T> = new Map();
+
+    public createNode(entity: Entity): T | null {
+        const node: Node = {};
+        for (const key of this.NODE_COMPONENT_KEYS) {
+            if (!entity.getComponentMap().has(key))
+                return null;
+            node[key] = entity.getComponent(key)!;
+        }
+        if (node)
+            this.addNode(entity.getID(), node as T);
+        return node as T;
+    }
+
+    public validateEntity(entity: Entity): boolean {
+        if (entity.hasComponents(Array.from(this.NODE_COMPONENT_KEYS)))
+            return true;
+        this.removeNode(entity.getID());
+        return false;
+    }
+    public hasNode(entityID: EntityID): boolean {
+        return this.nodeMap.has(entityID);
+    }
+    public getNode(entityID: EntityID): Node | undefined {
+        return this.nodeMap.get(entityID);
+    }
+    public addNode(entityID: EntityID, node: T) {
+        this.nodeMap.set(entityID, node);
+    }
+    public removeNode(entityID: EntityID): boolean {
+        return this.nodeMap.delete(entityID);
+    }
+    public getNodeMap(): Map<EntityID, T> {
+        return this.nodeMap;
+    }
+    public update(): void {
+        this.nodeMap.forEach(this.updateNode);
+    }
+    public abstract updateNode(node: T, entityID: EntityID): void;
+}
+
+/* 
+
+Later: System management
+Prioritized list
+plus phases: (preupdate, update, postupdate, prerender, render (world is rendered here), postrender (hud is rendered here))
+*/
+
+export class Engine {
+    private entityMap: Map<EntityID, Entity> = new Map();
+    private systemList: System<any>[] = [];
+    public addSystem(...system: System<any>[]): void {
+        this.systemList.push(...system);
+    }
+    public addEntityComponents(entity: Entity, ...components: Component[]): void {
+        components.forEach(component => entity.addComponent(component)); // Add all of the components to the entity
+        let componentKeySet = new Set<string>(components.map(c => c.key)); // Create a set of the keys of the components that have been added
+        for (let system of this.systemList) {
+            // If any of the system's node component keys are present (same component type), then the system is relevant.
+            if (!Array.from(system.NODE_COMPONENT_KEYS).some(componentKey => componentKeySet.has(componentKey)))
+                continue;
+            // If the system already has a node for this entity, remove it. A new node will be created to include the newly added component. This keeps the node up to date
+            if (system.hasNode(entity.getID()))
+                system.removeNode(entity.getID());
+            // Create the node if all the components are present.
+            system.createNode(entity);
+        }
+    }
+    public removeEntityComponents(entity: Entity, ...componentKeys: string[]): void {
+        // call system validate after removing components
+    }
+    public addEntity(entity: Entity): void {
+        this.entityMap.set(entity.getID(), entity);
+    }
+    public removeEntity(entityID: EntityID): void {
+        this.entityMap.delete(entityID);
+        this.systemList.forEach(system => system.removeNode(entityID));
+    }
+    public createEntity(...components: Component[]): Entity {
+        let entity = new Entity();
+        this.addEntityComponents(entity, ...components);
+        this.addEntity(entity);
+        return entity;
+    }
+    public update() { }
+    public start() { }
+    public stop() { }
+}
+
+
+let instance: Engine = null;
+
+export function getInstance(): Engine {
+    if (!instance)
+        throw new Error("An Engine instance has not been created!");
+
+    return instance;
+}
+
+export function setInstance(newInstance: Engine) {
+    if (instance)
+        throw new Error("An Engine instance has already been created!");
+    instance = newInstance;
+}
