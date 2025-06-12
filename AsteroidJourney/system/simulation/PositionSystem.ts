e8075e19fbda6d8fa9dc65e3f494b368bf08217c
Del <aelbanna3896@gmail.com>
2025-06-12 14:10:45 -0400

    update and refactor PositionSystem

--- AsteroidJourney/system/simulation/PositionSystem.ts
+++ AsteroidJourney/system/simulation/PositionSystem.ts
@@ -2,6 +2,8 @@ import { engine } from "../../AsteroidJourney.js";
 import { EntityID, System, MathUtils } from "../../../engine/FluidECS.js";
 import { PositionComponent, VelocityComponent } from "../../Components.js";
 
+const PI = Math.PI, PI2 = 2 * PI;
+
 export type PositionSystemNode = {
     position: PositionComponent;
     velocity: VelocityComponent;
@@ -11,8 +13,20 @@ export class PositionSystem extends System<PositionSystemNode> {
     NODE_COMPONENT_KEYS: Set<keyof PositionSystemNode> = new Set(['position', 'velocity']);
     public updateNode(node: PositionSystemNode, entityID: EntityID) {
         const DELTA_TIME = engine.getDeltaTime();
-        node.position.position.x += node.velocity.velocity.x * DELTA_TIME;
-        node.position.position.y += node.velocity.velocity.y * DELTA_TIME;
-        node.position.rotation += node.velocity.angular * DELTA_TIME;
+        const { position: posComp, velocity: velComp } = node;
+        const { position: pos } = posComp;
+        const { velocity: vel, angular: angVel } = velComp;
+        let rot = posComp.rotation;
+
+        pos.x += vel.x * DELTA_TIME;
+        pos.y += vel.y * DELTA_TIME;
+        rot += angVel * DELTA_TIME;
+
+        if (rot >= PI)
+            rot -= PI2;
+        if (rot < -PI)
+            rot += PI2;
+
+        posComp.rotation = rot;
     }
 }
