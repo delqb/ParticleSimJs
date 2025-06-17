2951b726968bcd3c83e1540d43af81920e035a79
Del <aelbanna3896@gmail.com>
2025-06-16 21:59:15 -0400

    Refactor OBB and implement factory function. - Implement explicit type 'Axes' to store OBB x and y axes. - Implement 'createOBB' factory function to construct OBB instances.

--- engine/src/lib/spatial/OBB.ts
+++ engine/src/lib/spatial/OBB.ts
@@ -1,7 +1,17 @@
-import { Vec2 } from "./Vector2";
+import { Vec2, Vector2 } from "./Vector2";
+
+export type Axes = {
+    x: Vec2;
+    y: Vec2;
+}
 
 export type OBB = {
     halfExtents: Vec2;      // Half-width and half-height along local axes
-    axes: [Vec2, Vec2];     // Local x and y axes (unit vectors), derived from rotation
+    axes: Axes;     // Local x and y axes (unit vectors), derived from rotation
     corners?: [Vec2, Vec2, Vec2, Vec2];        // Precomputed world-space corners (4 Vec2s)
+}
+
+
+export function createOBB({ halfExtents = Vector2.zero(), axes = { x: Vector2.zero(), y: Vector2.zero() }, corners = [Vector2.zero(), Vector2.zero(), Vector2.zero(), Vector2.zero()] } = {}): OBB {
+    return { halfExtents, axes, corners } as OBB;
 }
