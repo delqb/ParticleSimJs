98ddf8ff1bbdbc1687e82d58e10f7818c4544ef6
Del <aelbanna3896@gmail.com>
2025-06-17 20:54:34 -0400

    Rename 'AABB' properties from 'left', 'top', 'right', 'bottom' to 'minX', 'minY', 'maxX', 'maxY' to accurately represent AABB sides in world-space

--- engine/src/lib/spatial/AABB.ts
+++ engine/src/lib/spatial/AABB.ts
@@ -1,10 +1,10 @@
 import { RectSize } from "./RectSize";
 
 export type AABB = {
-    top: number;
-    bottom: number;
-    left: number;
-    right: number;
+    maxY: number;
+    minY: number;
+    minX: number;
+    maxX: number;
 }
 
 
@@ -12,26 +12,35 @@ export function createAABB(left: number, bottom: number, right: number, top: num
 export function createAABB(left: number, bottom: number, rectSize: RectSize): AABB;
 
 export function createAABB(
-    left: number,
-    bottom: number,
-    rightOrSize: number | RectSize,
-    top?: number
+    minX: number,
+    maxY: number,
+    maxXOrSize: number | RectSize,
+    minY?: number
 ): AABB {
-    if (typeof rightOrSize === "number" && typeof top === "number") {
+    if (typeof maxXOrSize === "number" && typeof minY === "number") {
         return {
-            left,
-            bottom,
-            right: rightOrSize,
-            top,
+            minX: minX,
+            minY: maxY,
+            maxX: maxXOrSize,
+            maxY: minY,
         };
-    } else if (typeof rightOrSize === "object" && rightOrSize !== null) {
+    } else if (typeof maxXOrSize === "object" && maxXOrSize !== null) {
         return {
-            left,
-            bottom,
-            right: left + rightOrSize.width,
-            top: bottom + rightOrSize.height,
+            minX: minX,
+            minY: maxY,
+            maxX: minX + maxXOrSize.width,
+            maxY: maxY + maxXOrSize.height,
         };
     } else {
         throw new Error("Invalid arguments passed to createAABB");
     }
+}
+
+export function aabbsIntersect(a: AABB, b: AABB): boolean {
+    return (
+        a.minX <= b.maxX &&
+        a.maxX >= b.minX &&
+        a.maxY >= b.minY &&
+        a.minY <= b.maxY
+    );
 }
