5e4d45be49bd2a2b19942643ea638cf601a01e30
Del <aelbanna3896@gmail.com>
2025-06-29 17:26:42 -0400

    Add newly designed archetype API modules

---
+++ FluidEngine/src/core/archetype/Archetype.ts
@@ -0,0 +1,9 @@
+import { ECSComponentType } from "../component";
+
+export interface ECSArchetype {
+    equals(other: ECSArchetype): boolean;
+    isSuperSetOf(other: ECSArchetype): boolean;
+    has<T>(componentType: ECSComponentType<T>): boolean;
+    with<T>(componentType: ECSComponentType<T>): ECSArchetype;
+    without<T>(componentType: ECSComponentType<T>): ECSArchetype;
+}
