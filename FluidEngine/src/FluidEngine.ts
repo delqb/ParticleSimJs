42f036d59731b59ba9ae02cec6fbf036a1d27634
Del <aelbanna3896@gmail.com>
2025-06-29 17:32:32 -0400

    Refactor 'FluidEngine' to use new 'Core' API

--- FluidEngine/src/FluidEngine.ts
+++ FluidEngine/src/FluidEngine.ts
@@ -1,27 +1,30 @@
-import {FluidCore} from "./FluidECS";
-import {FPSTimer} from "./lib/utils/FPSTimer";
+import { Core } from "./core";
+import { FPSTimer } from "./lib/utils/FPSTimer";
 
-export class FluidEngine extends FluidCore {
+export class FluidEngine {
     private isAnimating = false;
     private gameTime = 0;
     public readonly fpsTimer: FPSTimer;
-    constructor(public PIXELS_PER_METER: number = 1000, public deltaTime: number = 1 / 60, FPS_SAMPLING_INTERVAL: number = 20) {
-        super();
+    constructor(public core: Core, public PIXELS_PER_METER: number = 1000, public deltaTime: number = 1 / 60, FPS_SAMPLING_INTERVAL: number = 20) {
         this.fpsTimer = new FPSTimer(FPS_SAMPLING_INTERVAL);
     }
+
     getFPS() {
         return this.fpsTimer.getFPS();
     }
+
     getGameTime() {
         return this.gameTime;
     }
+
     getDeltaTime() {
         return this.deltaTime;
     }
+
     animate() {
         try {
             this.fpsTimer.tick();
-            this.update();
+            this.core.update();
         } catch (err) {
             console.error(err);
         }
@@ -29,18 +32,22 @@ export class FluidEngine extends FluidCore {
             requestAnimationFrame(this.animate.bind(this));
         this.gameTime += this.deltaTime;
     }
+
     getAnimationState() {
         return this.isAnimating;
     }
+
     startAnimation() {
         if (this.isAnimating)
             return;
         this.isAnimating = true;
         this.animate();
     }
+
     stopAnimation() {
         this.isAnimating = false;
     }
+
     toggleAnimation() {
         if (this.isAnimating)
             this.stopAnimation();
