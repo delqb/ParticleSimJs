f99a084ce67a2bd7f63b00aca0c2b9bebbef37bd
Del <aelbanna3896@gmail.com>
2025-06-30 04:18:20 -0400

    Expose more entity API modules through ECSEntityManager

--- FluidEngine/src/core/entity/EntityManager.ts
+++ FluidEngine/src/core/entity/EntityManager.ts
@@ -1,7 +1,14 @@
-import {ECSEntityId} from "./EntityId";
-import {ECSEntityProxy} from "./EntityProxy";
+import { ECSEntityFactory } from "./EntityFactory";
+import { ECSEntityId } from "./EntityId";
+import { ECSEntityProxy } from "./EntityProxy";
+import { ECSEntityProxyFactory } from "./EntityProxyFactory";
+import { ECSEntityResolver } from "./EntityResolver";
 
 export interface ECSEntityManager {
+    getEntityFactory(): ECSEntityFactory;
+    getEntityProxyFactory(): ECSEntityProxyFactory;
+    getEntityResolver(): ECSEntityResolver;
+
     getEntities(): Iterable<ECSEntityId>;
 
     hasEntity(entityId: ECSEntityId): boolean;
