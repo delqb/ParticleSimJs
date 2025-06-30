f11095154b737f8df97c18907b84dd6d07f33a13
Del <aelbanna3896@gmail.com>
2025-06-30 04:24:18 -0400

    Add optional updateNode method in ECSSystem

--- FluidEngine/src/core/system/System.ts
+++ FluidEngine/src/core/system/System.ts
@@ -1,8 +1,9 @@
-import {ECSNode} from "../node/Node";
-import {ECSNodeSchema} from "../node/schema/NodeSchema";
-import {ECSSystemMeta} from "./SystemMeta";
+import { ECSNode } from "../node/Node";
+import { ECSNodeSchema } from "../node/schema/NodeSchema";
+import { ECSSystemMeta } from "./SystemMeta";
 
 export interface ECSSystem<S extends ECSNodeSchema> {
     getSystemMeta(): ECSSystemMeta;
-    updateNodes(nodes: Iterable<ECSNode<S>>): void;
+    updateNodes(nodes: Iterable<ECSNode<ECSNodeSchema>>): void;
+    updateNode?(node: ECSNode<S>): void;
 }
--- FluidEngine/src/impl/core/system/FluidSystem.ts
+++ FluidEngine/src/impl/core/system/FluidSystem.ts
@@ -1,17 +1,29 @@
 import { ECSNode } from "@fluid/core/node/Node";
 import { ECSNodeSchema } from "@fluid/core/node/schema/NodeSchema";
+import { ECSNodeSchemaMeta } from "@fluid/core/node/schema/NodeSchemaMeta";
 import { ECSSystem } from "@fluid/core/system/System";
 import { ECSSystemMeta } from "@fluid/core/system/SystemMeta";
+import { FluidSystemMeta } from "./FluidSystemMeta";
 
 export abstract class FluidSystem<S extends ECSNodeSchema> implements ECSSystem<S> {
+    private readonly systemMeta: ECSSystemMeta;
     constructor(
-        private readonly systemMeta: ECSSystemMeta
+        name: string,
+        nodeSchemaMeta: ECSNodeSchemaMeta
     ) {
+        this.systemMeta = new FluidSystemMeta(name, nodeSchemaMeta);
     }
 
     getSystemMeta(): ECSSystemMeta {
         return this.systemMeta;
     }
 
-    abstract updateNodes(nodes: Iterable<ECSNode<S>>): void;
+    updateNode?(node: ECSNode<S>): void;
+
+    updateNodes(nodes: Iterable<ECSNode<S>>): void {
+        for (const node of nodes) {
+            if (this.updateNode)
+                this.updateNode(node);
+        }
+    };
 }
