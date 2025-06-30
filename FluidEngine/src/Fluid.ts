81dfe85c08b283d488ee8f12e94fe5aa1da8b3db
Del <aelbanna3896@gmail.com>
2025-06-30 04:28:22 -0400

    Add more utility methods to the API accessor

--- FluidEngine/src/Fluid.ts
+++ FluidEngine/src/Fluid.ts
@@ -1,5 +1,11 @@
-import { Core, CoreRuntime } from ".";
+import { Core } from "./core/Core";
+import { CoreRuntime } from "./core/CoreRuntime";
+import { ECSComponent } from "./core/component/Component";
 import { ECSComponentType } from "./core/component/type/ComponentType";
+import { ECSEntityId } from "./core/entity/EntityId";
+import { ECSEntityProxy } from "./core/entity/EntityProxy";
+import { ECSNodeSchema } from "./core/node/schema/NodeSchema";
+import { ECSNodeSchemaMeta } from "./core/node/schema/NodeSchemaMeta";
 
 export class Fluid {
     static core(): Core {
@@ -19,4 +25,33 @@ export class Fluid {
         return componentType;
     }
 
+    static registerNodeSchema<S extends ECSNodeSchema>(nodeSchema: S, name: string): ECSNodeSchemaMeta {
+        return this.core().getNodeManager().getNodeSchemaRegistry().addSchema(nodeSchema, name);
+    }
+
+    static getEntityProxy(entityId: ECSEntityId): ECSEntityProxy {
+        return this.core().getEntityManager().getEntityProxy(entityId);
+    }
+
+    static removeEntity(entityId: ECSEntityId): void {
+        this.core().getEntityManager().removeEntity(entityId);
+        this.core().getComponentManager().getComponentRepository().removeEntityComponents(entityId);
+    }
+
+    static addEntityComponent<T>(entityId: ECSEntityId, component: ECSComponent<T>): void {
+        this.core().getComponentManager().getComponentRepository().addComponent(component, entityId);
+    }
+
+    static addEntityComponents(entityId: ECSEntityId, ...components: ECSComponent<any>[]) {
+        const componentRepo = this.core().getComponentManager().getComponentRepository();
+        for (const component of components) {
+            componentRepo.addComponent(component, entityId);
+        }
+    }
+
+    static createEntityWithComponents(...components: ECSComponent<any>[]): ECSEntityId {
+        const entityId = this.core().getEntityManager().createEntity();
+        this.addEntityComponents(entityId, ...components);
+        return entityId;
+    }
 }
